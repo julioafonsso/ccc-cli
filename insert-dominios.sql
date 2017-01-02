2932accb2ee3ded884770da465101db59559175d
Julio Afonso <julioafonsso@gmail.com>
2017-01-02 21:30:13 -0200

    Inserts dos dominios

---
+++ insert-dominios.sql
@@ -0,0 +1,29 @@
+insert into dias_semana (id, nome) values(1, 'Domingo');
+insert into dias_semana (id, nome) values(2, 'Segunda-Feira');
+insert into dias_semana (id, nome) values(3, 'Terça-Feira');
+insert into dias_semana (id, nome) values(4, 'Quarta-Feira');
+insert into dias_semana (id, nome) values(5, 'Quinta-Feira');
+insert into dias_semana (id, nome) values(6, 'Sexta-Feira');
+insert into dias_semana (id, nome) values(7, 'Sábado');
+
+insert into conhece_escola (id, nome) values(1, 'Internet');
+insert into conhece_escola (id, nome) values(2, 'Site');
+insert into conhece_escola (id, nome) values(3, 'Amigos');
+insert into conhece_escola (id, nome) values(4, 'Baile');
+insert into conhece_escola (id, nome) values(5, 'Outros');
+
+insert into nivel_turma (id, nome) values(1, 'Iniciante');
+insert into nivel_turma (id, nome) values(2, 'Intermediario');
+insert into nivel_turma (id, nome) values(3, 'Avançado');
+
+insert into estado_civil (id, nome) values(1, 'Solteiro(a)');
+insert into estado_civil (id, nome) values(2, 'Casado(a)');
+insert into estado_civil (id, nome) values(3, 'Divorciado(a)');
+insert into estado_civil (id, nome) values(4, 'Viuvo(a)');
+
+insert into sala (id, nome) values(1, 'Sala 1');
+insert into sala (id, nome) values(2, 'Sala 2');
+insert into sala (id, nome) values(3, 'Sala 3');
+insert into sala (id, nome) values(4, 'Sala 4');
+insert into sala (id, nome) values(5, 'Sala 5');
+insert into sala (id, nome) values(6, 'Sala 6');
