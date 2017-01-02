32b868e1ee42fd29314c4a363fc1ec1274e34bf1
Julio Afonso <julioafonsso@gmail.com>
2017-01-02 21:43:48 -0200

    cadastrar professor

--- src/app/servicos/professor.service.ts
+++ src/app/servicos/professor.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 
@@ -11,13 +12,12 @@ export class ProfessorService {
   constructor(private http: Http) { }
 
   getProfessores(){
-    return this.http.get('json/professores.json')
+    return this.http.get(environment.url + "professores")
     .map((response: Response) => <Professor[]>response.json())
   }
 
-  getProfessoras(){
-    return this.http.get('json/professores.json')
-    .map((response: Response) => <Professor[]>response.json())
+  cadstrarProfessor(professor: Professor){
+    return this.http.post(environment.url + "/professores", professor);
   }
 
 }
