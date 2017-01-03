6367208c65c321522fdca8b73c4ba7b0cc0979a5
Julio Afonso <julioafonsso@gmail.com>
2017-01-02 22:16:24 -0200

    Cadastro de movimentacao de caixa

--- src/app/lancamento-movimentacao-caixa/lancamento-movimentacao-caixa.component.ts
+++ src/app/lancamento-movimentacao-caixa/lancamento-movimentacao-caixa.component.ts
@@ -1,7 +1,9 @@
+import { FluxoCaixaService } from './../servicos/fluxo-caixa.service';
 import { Component, OnInit } from '@angular/core';
 
 import { TipoFluxoCaixaService } from './../servicos/tipo-fluxo-caixa.service';
 import { TipoFluxo } from './../models/tipo-fluxo';
+import { FluxoCaixa } from './../models/fluxo-caixa';
 
 
 @Component({
@@ -14,17 +16,31 @@ export class LancamentoMovimentacaoCaixaComponent implements OnInit {
   private fluxos: TipoFluxo[];
   private fluxosEntrada: TipoFluxo[];
   private fluxosSaida: TipoFluxo[];
+  private fluxoCaixa = new FluxoCaixa();
 
-
-  constructor(private tipoFluxoService: TipoFluxoCaixaService) { }
+  constructor(private tipoFluxoService: TipoFluxoCaixaService, private fluxoCaixaService: FluxoCaixaService) { }
 
   ngOnInit() {
     this.tipoFluxoService.getTipoFluxoEntrada().subscribe(res => {
       this.fluxosEntrada = res;
-      this.fluxos = res;
     });
 
     this.tipoFluxoService.getTipoFluxoSaida().subscribe(res => { this.fluxosSaida = res })
   }
 
+  atualizarTipoFluxo(indEntrada) {
+    if(indEntrada == 'E'){
+      this.fluxos = this.fluxosEntrada;
+    }
+    else{
+      this.fluxos = this.fluxosSaida;
+    }
+
+    this.fluxoCaixa.tipoFluxo = new TipoFluxo();
+    
+  }
+
+  onSubmit(){
+    this.fluxoCaixaService.cadastrar(this.fluxoCaixa);
+  }
 }
--- src/app/servicos/fluxo-caixa.service.ts
+++ src/app/servicos/fluxo-caixa.service.ts
@@ -1,8 +1,17 @@
+import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+
+import { FluxoCaixa } from './../models/fluxo-caixa';
+import { environment } from './../../environments/environment';
+
 
 @Injectable()
 export class FluxoCaixaService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  cadastrar(fluxoCaixa: FluxoCaixa){
+    this.http.post(environment.url + "fluxo-caixa", fluxoCaixa)
+  }
 
 }
