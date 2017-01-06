3743d6de6fe7891fe62346bdc89f16b76ea9d713
Julio Afonso <julioafonsso@gmail.com>
2017-01-06 13:01:44 -0200

    inclusao dos novos modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CadastroModalidadeTurmaModule } from './cadastro-modalidade-turma/cadastro-modalidade-turma.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -16,25 +17,20 @@ import { ManutencaoProfessorModule } from './manutencao-professor/manutencao-pro
 import { LancamentoMovimentacaoCaixaModule } from './lancamento-movimentacao-caixa/lancamento-movimentacao-caixa.module';
 import { ConsultaTurmasModule } from './consulta-turmas/consulta-turmas.module';
 import { ConsultaAlunosDebitoModule } from './consulta-alunos-debito/consulta-alunos-debito.module';
-import { ConsultaAlunoModule } from './consulta-aluno/consulta-aluno.module';
+import { ConsultaAlunosModule } from './consulta-alunos/consulta-alunos.module';
 import { CadastroTurmaModule } from './cadastro-turma/cadastro-turma.module';
 import { CadastroTipoFluxoCaixaModule } from './cadastro-tipo-fluxo-caixa/cadasstro-tipo-fluxo-caixa.module';
 import { CadastroTipoDescontoModule } from './cadastro-tipo-desconto/cadastro-tipo-desconto.module';
 import { CadastroProfessorModule } from './cadastro-professor/cadastro-professor.module';
 import { CadastroAlunoModule } from './cadastro-aluno/cadastro-aluno.module';
+import { DetalheAlunoModule } from './detalhe-aluno/detalhe-aluno.module';
 
 
 import { routing } from './app.routing';
-import { CadastroModalidadeTurmaComponent } from './cadastro-modalidade-turma/cadastro-modalidade-turma.component';
-
-
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CadastroModalidadeTurmaComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -46,8 +42,9 @@ import { CadastroModalidadeTurmaComponent } from './cadastro-modalidade-turma/ca
     CadastroProfessorModule,
     CadastroTipoDescontoModule,
     CadastroTipoFluxoCaixaModule,
+    CadastroModalidadeTurmaModule,
     CadastroTurmaModule,
-    ConsultaAlunoModule,
+    ConsultaAlunosModule,
     ConsultaAlunosDebitoModule,
     ConsultaTurmasModule,
     LancamentoMovimentacaoCaixaModule,
@@ -55,6 +52,7 @@ import { CadastroModalidadeTurmaComponent } from './cadastro-modalidade-turma/ca
     ManutencaoTurmaModule,
     RelatorioMovimentacaoCaixaModule,
     RelatorioPagamentoProfessorModule,
+    DetalheAlunoModule,
     routing
   ],
   exports: [],
