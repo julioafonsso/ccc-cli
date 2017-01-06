906e97be9cebb99173b95ccd4338e6cb87745555
Julio Afonso <julioafonsso@gmail.com>
2017-01-06 11:16:12 -0200

    inclusao de module

---
+++ src/app/cadastro-modalidade-turma/cadastro-modalidade-turma.module.ts
@@ -0,0 +1,14 @@
+import { CadastroModalidadeTurmaComponent } from './cadastro-modalidade-turma.component';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [CadastroModalidadeTurmaComponent]
+})
+export class CadastroModalidadeTurmaModule { }
