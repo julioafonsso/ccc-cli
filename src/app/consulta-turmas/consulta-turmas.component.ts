d58a215bfecf9331be5e05aaaa1e30e22b932344
Julio Afonso <julioafonsso@gmail.com>
2017-01-06 12:59:45 -0200

    Inclusao da Consulta de turmas

--- src/app/consulta-turmas/consulta-turmas.component.ts
+++ src/app/consulta-turmas/consulta-turmas.component.ts
@@ -1,4 +1,16 @@
+import { ProfessorService } from './../servicos/professor.service';
+import { Response } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
+
+
+import { ModalidadeTurma } from './../models/modalidade-turma';
+import { NivelTurma } from './../models/nivel-turma';
+import { Professor } from './../models/professor';
+import { TurmaProfessor } from './../models/turma-professor';
+import { Turma } from './../models/turma';
+import { TurmaService } from './../servicos/turma.service';
+import { DiasSemana } from './../models/dias-semana';
+
 
 @Component({
   selector: 'app-consulta-turmas',
@@ -7,9 +19,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaTurmasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private turmaService: TurmaService, private professorService: ProfessorService ) { }
+
+  private niveis = new Array<NivelTurma>();
+  private modalidades = new Array<ModalidadeTurma>()
+  private turmas: Turma[];
+
+  private nivelSelecionado: NivelTurma;
+  private modalidadeSelecionado: ModalidadeTurma;
 
   ngOnInit() {
+    this.turmaService.getTurmas().subscribe(res => {
+      this.turmas = res;
+    })
+
+    this.turmaService.getNiveis().subscribe(res => {
+      this.niveis.push(new NivelTurma());
+      res.forEach(element => {
+        this.niveis.push(element);
+      });
+
+    })
+
+    this.turmaService.getModalidades().subscribe(res => {
+      this.modalidades.push(new ModalidadeTurma());
+      res.forEach(element => {
+        this.modalidades.push(element);
+      });
+    })
+  }
+
+  getTurmas() {
+    
+    let valores = this.turmas;
+    
+    if ((
+      this.nivelSelecionado != undefined
+      && this.nivelSelecionado.id != undefined)) {
+      valores = valores.filter((turma: Turma) =>
+        turma.nivel.id === this.nivelSelecionado.id);
+    }
+
+    if ((
+      this.modalidadeSelecionado != undefined
+      && this.modalidadeSelecionado.id != undefined)) {
+      valores = valores.filter((turma: Turma) =>
+        turma.modalidade.id === this.modalidadeSelecionado.id);
+    }
+
+    return valores;
+  }
+
+
+  
+
+
+  getDias(diasSemana: DiasSemana[]) {
+    return this.turmaService.getDias(diasSemana)
+  }
+
+  getProfessor(professores: TurmaProfessor[], index: number) {
+    return this.professorService.getProfessor(professores, index);
+
   }
 
 }
--- src/app/consulta-turmas/consulta-turmas.module.ts
+++ src/app/consulta-turmas/consulta-turmas.module.ts
@@ -1,12 +1,16 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ConsultaTurmasComponent } from './consulta-turmas.component';
+import { routing } from './../app.routing';
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    routing
   ],
   declarations: [ConsultaTurmasComponent]
 })
