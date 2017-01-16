e7cab0d839c049d182533ba15da9a91c59296c09
juliomarques6 <juliomarques6@gmail.com>
2017-01-16 00:02:52 -0200

    Cadastro tipo desconto

--- src/app/cadastro-modalidade-turma/cadastro-modalidade-turma.module.ts
+++ src/app/cadastro-modalidade-turma/cadastro-modalidade-turma.module.ts
@@ -1,7 +1,8 @@
-import { CadastroModalidadeTurmaComponent } from './cadastro-modalidade-turma.component';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { CadastroModalidadeTurmaComponent } from './cadastro-modalidade-turma.component';
 
 
 @NgModule({
