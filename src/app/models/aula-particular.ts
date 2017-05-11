3f647d628f476066e2613df7e09725466b57037f
Julio Afonso <julioafonsso@gmail.com>
2017-05-11 12:15:41 -0300

    inclusado do model de aula particular

---
+++ src/app/models/aula-particular.ts
@@ -0,0 +1,18 @@
+import { Turma } from './turma';
+import { Matricula } from './maticula';
+import { Aluno } from './aluno';
+import { Professor } from './professor';
+import { NivelTurma } from './nivel-turma';
+import { Salas } from './salas';
+import { DiasSemana } from './dias-semana';
+import { ModalidadeTurma } from './modalidade-turma';
+
+export class AulaParticular {
+
+    public turma: Turma;
+    public qtd: number;
+   
+    constructor() {
+   
+    }
+}
