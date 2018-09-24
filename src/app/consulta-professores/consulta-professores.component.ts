9a765bf30a9f623ad02e87af998069a8ab68fca7
julio <julioafonsso@gmail.com>
2018-09-24 20:42:25 -0300

    alteracao

--- src/app/consulta-professores/consulta-professores.component.ts
+++ src/app/consulta-professores/consulta-professores.component.ts
@@ -15,25 +15,38 @@ export class ConsultaProfessoresComponent implements OnInit {
   private professores: ConsultaProfessor[];
   private filtro: string;
 
-  constructor(private professorService: ProfessorService) { 
+  constructor(private professorService: ProfessorService) {
     this.professores = [];
     this.filtro = "";
   }
 
   ngOnInit() {
-    this.professorService.getProfessores().subscribe(res =>{
+    this.professorService.getProfessores().subscribe(res => {
       this.professores = res;
     })
   }
-  getProfessores(){
-     return this.professores.filter((professor) => {
-      if (professor.nome.toLowerCase().indexOf(this.filtro.toLowerCase()) > -1)
+  getProfessores() {
+    return this.professores.filter((professor) => {
+
+      if (this.validaFiltro(professor.nome))
         return true;
-      if (professor.cpf.toLowerCase().indexOf(this.filtro.toLowerCase()) > -1)
+      if (this.validaFiltro(professor.cpf))
         return true;
-      if (professor.email.toLowerCase().indexOf(this.filtro.toLowerCase()) > -1)
+      if (this.validaFiltro(professor.email))
         return true;
+
+
+      // if (professor.nome.toLowerCase().indexOf(this.filtro.toLowerCase()) > -1)
+      //   return true;
+      // if (professor.cpf.toLowerCase().indexOf(this.filtro.toLowerCase()) > -1)
+      //   return true;
+      // if (professor.email.toLowerCase().indexOf(this.filtro.toLowerCase()) > -1)
+      //   return true;
     });
+  }
+
+  validaFiltro(campo: string) {
+    return campo != undefined && campo.toLowerCase().indexOf(this.filtro.toLowerCase())
   }
 
 }
